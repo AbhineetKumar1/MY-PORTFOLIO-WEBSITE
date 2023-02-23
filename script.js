a890f9f5db44f865fc26fd6da6d42650f7d8b05b
AbhineetKumar1 <116869667+AbhineetKumar1@users.noreply.github.com>
2023-02-23 14:04:25 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Fullstack web Developer","Adventurer","Explorer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
